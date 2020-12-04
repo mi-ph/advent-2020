df7b960d4f39ee18177c0d824bfe3d19fd14c365
Michael Phillips <miph@fastmail.com>
2020-12-04 23:39:09 +1100

    Condensed the code, fixed some names in day 4

--- 4-passport-processing/program.c
+++ 4-passport-processing/program.c
@@ -8,163 +8,112 @@ int main(void)
 {
     char line[MAX_LINE];
 
-    char param[4];
+    char field[4];
     char value[MAX_LINE];
 
-    int validPassports = 0;
-    int allFieldPassports = 0;
-    int field[7] = {0, 0, 0, 0, 0, 0, 0};
-    int valid[7] = {0, 0, 0, 0, 0, 0, 0};
+    int numPassportsValid = 0;
+    int numPassportsWithAllRequiredFields = 0;
+    int hasField[7] = {0, 0, 0, 0, 0, 0, 0};
+    int isValid[7] = {0, 0, 0, 0, 0, 0, 0};
+
+    int i, j;
 
     while(fgets(line, MAX_LINE, stdin))
     {
-        for(int i = 0; line[i] != '\0'; i++)
+        for(i = 0; line[i] != '\0'; i++)
         {
             if(line[i] == ':')
             {
-                param[0] = line[i-3];
-                param[1] = line[i-2];
-                param[2] = line[i-1];
-                param[3] = '\0';
-                int j = 1;
-                while(line[i+j] != ' ' && line[i+j] != '\n')
+                int identifierIndex = i;
+                line[i] = '\0';
+                strcpy(field, &line[identifierIndex-3]);
+
+                for(; line[i] != ' ' && line[i] != '\n'; i++);
+                line[i] = '\0';
+                strcpy(value, &line[identifierIndex+1]);
+
+                if(!strcmp(field, "byr"))
                 {
-                    value[j-1] = line[i+j];
-                    j++;
-                }
-                value[j-1] = '\0';
-                if(!strcmp(param, "byr"))
-                {
-                    field[0] = 1;
+                    hasField[0] = 1;
                     int numDigits = 0;
-                    j = 0;
-                    while(value[j] != '\0')
+                    for(j = 0; value[j] != '\0' && value[j] >= '0' && value[j] <= '9'; j++) numDigits++;
+                    if(value[j] == '\0' && numDigits == 4)
                     {
-                        if(value[j] >= '0' && value[j] <= '9')
-                        {
-                            numDigits++;
-                        } else {
-                            numDigits = -12345;
-                        }
-                        j++;
-                    }
-                    if(numDigits == 4) {
                         int birthYear = atoi(value);
-                        if(birthYear >= 1920 && birthYear <= 2002)
-                        {
-                            valid[0] = 1;
-                        }
+                        if(birthYear >= 1920 && birthYear <= 2002) isValid[0] = 1;
                     }
-                } else if(!strcmp(param, "iyr")) {
-                    field[1] = 1;
+                } else if(!strcmp(field, "iyr")) {
+                    hasField[1] = 1;
                     int numDigits = 0;
-                    j = 0;
-                    while(value[j] != '\0')
+                    for(j = 0; value[j] != '\0' && value[j] >= '0' && value[j] <= '9'; j++) numDigits++;
+                    if(value[j] == '\0' && numDigits == 4)
                     {
-                        if(value[j] >= '0' && value[j] <= '9')
-                        {
-                            numDigits++;
-                        } else {
-                            numDigits = -12345;
-                        }
-                        j++;
-                    }
-                    if(numDigits == 4) {
                         int issueYear = atoi(value);
-                        if(issueYear >= 2010 && issueYear <= 2020)
-                        {
-                            valid[1] = 1;
-                        }
+                        if(issueYear >= 2010 && issueYear <= 2020) isValid[1] = 1;
                     }
-                } else if(!strcmp(param, "eyr")) {
-                    field[2] = 1;
+                } else if(!strcmp(field, "eyr")) {
+                    hasField[2] = 1;
                     int numDigits = 0;
-                    j = 0;
-                    while(value[j] != '\0')
+                    for(j = 0; value[j] != '\0' && value[j] >= '0' && value[j] <= '9'; j++) numDigits++;
+                    if(value[j] == '\0' && numDigits == 4)
                     {
-                        if(value[j] >= '0' && value[j] <= '9')
-                        {
-                            numDigits++;
-                        } else {
-                            numDigits = -12345;
-                        }
-                        j++;
-                    }
-                    if(numDigits == 4) {
                         int expirationYear = atoi(value);
-                        if(expirationYear >= 2020 && expirationYear <= 2030)
-                        {
-                            valid[2] = 1;
-                        }
+                        if(expirationYear >= 2020 && expirationYear <= 2030) isValid[2] = 1;
                     }
-                } else if(!strcmp(param, "hgt")) {
-                    field[3] = 1;
-                    j = 0;
-                    while(value[j] != '\0' && value[j] >= '0' && value[j] <= '9')
-                    {
-                        j++;
-                    }
+                } else if(!strcmp(field, "hgt")) {
+                    hasField[3] = 1;
+                    for(j = 0; value[j] != '\0' && value[j] >= '0' && value[j] <= '9'; j++);
                     if(!strcmp(&value[j], "cm") && j != 0)
                     {
                         value[j] = '\0';
                         int height = atoi(value);
-                        if(height >= 150 && height <= 193) valid[3] = 1;
+                        if(height >= 150 && height <= 193) isValid[3] = 1;
                     }
                     if(!strcmp(&value[j], "in") && j != 0)
                     {
                         value[j] = '\0';
                         int height = atoi(value);
-                        if(height >= 59 && height <= 76) valid[3] = 1;
+                        if(height >= 59 && height <= 76) isValid[3] = 1;
                     }
-                } else if(!strcmp(param, "hcl")) {
-                    field[4] = 1;
+                } else if(!strcmp(field, "hcl")) {
+                    hasField[4] = 1;
                     if(value[0] == '#')
                     {
                         int colourLength = 1;
                         while((value[colourLength] >= '0' && value[colourLength] <= '9') || (value[colourLength] >= 'a' && value[colourLength] <= 'f')) colourLength++;
-                        if(colourLength == 7)
-                        {
-                            valid[4] = 1;
-                        }
+                        if(colourLength == 7) isValid[4] = 1;
                     }
-                } else if(!strcmp(param, "ecl")) {
-                    field[5] = 1;
-                    if(!(strcmp(value, "amb") && strcmp(value, "blu") && strcmp(value, "brn") && strcmp(value, "gry") && strcmp(value, "grn") && strcmp(value, "hzl") && strcmp(value, "oth")))
-                    {
-                        valid[5] = 1;
-                    }
-                } else if(!strcmp(param, "pid")) {
-                    field[6] = 1;
+                } else if(!strcmp(field, "ecl")) {
+                    hasField[5] = 1;
+                    if(!(strcmp(value, "amb") && strcmp(value, "blu") && strcmp(value, "brn") && strcmp(value, "gry") && strcmp(value, "grn") && strcmp(value, "hzl") && strcmp(value, "oth"))) isValid[5] = 1;
+                } else if(!strcmp(field, "pid")) {
+                    hasField[6] = 1;
                     int passportLength = 0;
                     while(value[passportLength] >= '0' && value[passportLength] <= '9') passportLength++;
-                    if(passportLength == 9) {
-                        valid[6] = 1;
-                    }
+                    if(passportLength == 9) isValid[6] = 1;
                 }
             }
         }
         if(line[0] == '\n')
         {
-            int validFields = 0;
-            int presentFields = 0;
-            for(int i = 0; i < 7; i++) validFields += valid[i];
-            for(int i = 0; i < 7; i++) valid[i] = 0;
-            if(validFields == 7) validPassports++;
-            for(int i = 0; i < 7; i++) presentFields += field[i];
-            for(int i = 0; i < 7; i++) field[i] = 0;
-            if(presentFields == 7) allFieldPassports++;
+            int numValidFields = 0;
+            int numFields = 0;
+            for(int i = 0; i < 7; i++) numValidFields += isValid[i];
+            for(int i = 0; i < 7; i++) isValid[i] = 0;
+            if(numValidFields == 7) numPassportsValid++;
+            for(int i = 0; i < 7; i++) numFields += hasField[i];
+            for(int i = 0; i < 7; i++) hasField[i] = 0;
+            if(numFields == 7) numPassportsWithAllRequiredFields++;
         }
     }
-    int validFields = 0;
-    int presentFields = 0;
-    for(int i = 0; i < 7; i++) validFields += valid[i];
-    for(int i = 0; i < 7; i++) valid[i] = 0;
-    if(validFields == 7) validPassports++;
-    for(int i = 0; i < 7; i++) presentFields += field[i];
-    for(int i = 0; i < 7; i++) field[i] = 0;
-    if(presentFields == 7) allFieldPassports++;
+    int numValidFields = 0;
+    int numFields = 0;
+    for(int i = 0; i < 7; i++) numValidFields += isValid[i];
+    if(numValidFields == 7) numPassportsValid++;
+    for(int i = 0; i < 7; i++) numFields += hasField[i];
+    if(numFields == 7) numPassportsWithAllRequiredFields++;
 
-    printf("%d passports had all 7 required fields.\n", allFieldPassports);
-    printf("%d passports were valid.\n", validPassports);
+    printf("%d passports had all 7 required fields.\n", numPassportsWithAllRequiredFields);
+    printf("%d passports were valid.\n", numPassportsValid);
     return 0;
 }
