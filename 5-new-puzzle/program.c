712488c8ad8da64ff8d6252add36505a1deed1af
Michael Phillips <miph@fastmail.com>
2020-12-05 16:42:41 +1100

    Puzzle 5: seat id optimisation

--- 5-new-puzzle/program.c
+++ 5-new-puzzle/program.c
@@ -3,26 +3,19 @@
 #define MAX_LINE 4096
 
 int main(void) {
-    int filledSeats[1024];
     char line[MAX_LINE];
     int biggestId = 0;
+    int filledSeats[1024];
 
     for(int i = 0; i < 1024; i++) filledSeats[i] = 0;
     while(fgets(line, MAX_LINE, stdin))
     {
-        int row = 0;
-        int multiplier = 64;
-        for(int i = 0; i < 7; i++) {
-            if(line[i] == 'B') row += multiplier;
+        int id = 0;
+        int multiplier = 512;
+        for(int i = 0; i < 10; i++) {
+            if(line[i] == 'B' || line[i] == 'R') id += multiplier;
             multiplier /= 2;
         }
-        int col = 0;
-        multiplier = 4;
-        for(int i = 7; i < 10; i++) {
-            if(line[i] == 'R') col += multiplier;
-            multiplier /= 2;
-        }
-        int id = 8 * row + col;
         if (id > biggestId) biggestId = id;
         filledSeats[id] = 1;
     }
