4b1c113059cab3408f613e24bec4545273db38dd
Michael Phillips <miph@fastmail.com>
2020-12-07 10:18:24 +1100

    Changed loop condition to cover the exit iteration in day 6

--- 6-custom-customs/program.c
+++ 6-custom-customs/program.c
@@ -8,11 +8,14 @@ int main(void) {
     int allSaidYes = 0;
     int questions[26];
     int N = 0;
+    char* newInput = line;
 
     for(int i = 0; i < 26; i++) questions[i] = 0;
-    while(fgets(line, MAX_LINE, stdin))
+
+    while(newInput)
     {
-        if(line[0] == '\n') {
+        newInput = fgets(line, MAX_LINE, stdin);
+        if(line[0] == '\n' || !newInput) {
             for(int i = 0; i < 26; i++)
             {
                 oneSaidYes += (questions[i] > 0);
@@ -21,18 +24,9 @@ int main(void) {
             }
             N = 0;
         } else {
-            for(int i = 0; line[i] != '\n'; i++)
-            {
-                questions[line[i] - 'a'] += 1;
-            }
+            for(int i = 0; line[i] != '\n'; i++) questions[line[i] - 'a'] += 1;
             N++;
         }
-    }
-    for(int i = 0; i < 26; i++)
-    {
-        oneSaidYes += (questions[i] > 0);
-        allSaidYes += (questions[i] == N);
-        questions[i] = 0;
     }
     printf("At least one said yes: %d\n", oneSaidYes);
     printf("All said yes: %d\n", allSaidYes);
