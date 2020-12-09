62e1cf84471fee6bb63d70e64f27867989e47482
Michael Phillips <miph@fastmail.com>
2020-12-09 16:23:46 +1100

    Puzzle 9 - encoding error solved. Python, it's just faster to write than C

---
+++ 9-encoding-error/program.py
@@ -0,0 +1,39 @@
+def process(line):
+    k = line.split(' ')[0]
+    v = int(line.split(' ')[1])
+    return (k, v)
+
+def last25(array, i):
+    return array[i-25:i]
+
+def find(num, array):
+    for i in range(len(array)):
+        for j in range(len(array)):
+            if array[i] + array[j] == num:
+                return False
+    return True
+
+with open("input.txt", "r") as fh:
+    lines = fh.readlines()
+    array = []
+    for line in lines:
+        array.append(int(line))
+
+    error = 0
+    for trial in range(25,len(array)):
+        if find(array[trial], last25(array, trial)):
+            error = array[trial]
+            print(f"Puzzle 1: {error}")
+
+    begin = 0
+    end = 0
+    while True:
+        theSlice = array[begin:end+1]
+        theSum = sum(theSlice)
+        if theSum == error:
+            print(f"Puzzle 2: {min(theSlice) + max(theSlice)}")
+            break
+        if theSum > error:
+            begin += 1
+        if theSum < error:
+            end += 1
